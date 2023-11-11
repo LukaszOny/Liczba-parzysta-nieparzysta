c2b866afb60c2eb722aa35f636554060b0b57523
lonys <lonys@ONYSZEK>
2023-11-11 14:19:28 +0100

    Add project files.

---
+++ Liczba parzysta nieparzysta/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Wprowadż dowolną liczbę, aby sprawdzić czy jest ona parzystą");
+int a = Int32.Parse(Console.ReadLine());
+
+if (a%2 == 0)
+{
+    Console.WriteLine($"Liczba {a} jest liczbą parzystą");
+
+}
+else
+{
+    Console.WriteLine($"Liczba {a} jest liczbą nieparzystą");
+}
+
